0d2579aaab177f2d8c8dc06b357013a149038b08
supr1yo <supriyo.developer@gmail.com>
2024-09-15 17:12:28 +0530

    Add bubble sort algo

--- Python/Arrays/functions/bubble_sort.py
+++ Python/Arrays/functions/bubble_sort.py
@@ -1,15 +1,27 @@
+def bubble_sort(arr: list, ascending: bool = True) -> list:
+    '''Bubble Sort is an algorithm that sorts an array from the lowest value to the highest value.'''
+    
+    if ascending is True:
+        size = len(arr) # Length of array
 
-def bubble_sort(arr: list) -> list:
-    size = len(arr) # Length of array
+        for i in range(size):
+            swapped = False
+            for j in range(size - i - 1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+            if not swapped:
+                break
+    else:
+        size = len(arr) # Length of array
 
-
-    for i in range(size):
-        swapped = False
-        for j in range(size - i - 1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-        if not swapped:
-            break
+        for i in range(size):
+            swapped = False
+            for j in range(size - i - 1):
+                if arr[j] < arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+            if not swapped:
+                break
     
     return arr
