086851b1c3f4abb7a521af776208af5c51ee14c0
supr1yo <supriyo.developer@gmail.com>
2024-09-20 11:36:43 +0530

    stack

--- C/stack.c
+++ C/stack.c
@@ -1,15 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define CAPACITY 100
 
-int top = -1, stack[10];
+typedef struct Stack {
+    int top;
+    int arr[CAPACITY];
+} Stack;
 
-void push(int val){
-    stack[++top] = val;
+// Init stack
+void initStack(Stack *stack) {
+    stack->top = -1;
 }
-void pop(){
-    top--;
+    
+// Check if stack is empty
+int isEmpty(Stack *stack) {
+    return stack->top = -1;
 }
 
+// Check if stack is full
+int isFull(Stack *stack) {
+    return stack->top = CAPACITY - 1;
+}
 
+// Push to the stack
+void push(Stack* stack, int val){
+    if(!isFull(stack)) {
+        stack->arr[++stack->top] = val;
+    } else {
+        printf("Stack overflow.\n");
+    }
+}
 
-int main() {}
+// Pop from the stack
+void pop(Stack* stack) {
+    if(!isEmpty(stack)) {
+        stack->arr[stack->top--];
+    } else {
+        printf("Stack underflow.\n");
+    }
+}
+
+// Peek
+void peek(Stack* stack) {
+    stack->arr[stack->top];
+}
+
+void display(Stack* stack) {
+    
+}
+
+int main() {
+    Stack stack;
+    push(&stack, 10);
+}
