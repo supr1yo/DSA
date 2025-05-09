48ffaa7408d3bf83ad963d9efc17bb09133475b4
supr1yo <supriyo.developer@gmail.com>
2025-05-09 08:43:15 +0530

    feat: Bubble sort algo

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+
+    static void Sort(int[] arr, int n) {
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,5,4};
+        int n = 5;
+
+        Sort(arr, n);
+
+        // Printing the sorted array:
+        System.out.println("Printing the sorted array:");
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
